a20c0153b68e0998cfc487980ed2f4f291a46e22
Lhester Monroyo <lhestermoon3011@gmail.com>
2023-11-28 12:21:27 +0800

    add dark and light mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 
 import Header from '@/components/Header';
+import Providers from './Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,14 +15,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/*  header */}
-        <Header />
+        <Providers>
+          {/*  header */}
+          <Header />
 
-        {/* navbar */}
+          {/* navbar */}
 
-        {/* searchbox */}
+          {/* searchbox */}
 
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 import { AiFillHome } from 'react-icons/ai';
 import { BsFillInfoCircleFill } from 'react-icons/bs';
 import MenuItem from './MenuItem';
-import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch';
 
 export default function Header() {
   return (
-    <div className="flex justify-between p-2 max-w-6xl sm:mx-auto items-center">
-      <div className="">
+    <div className="flex justify-between px-2 py-4 max-w-6xl sm:mx-auto items-center">
+      <div className="flex items-center space-x-4">
         <Link href="/">
           <h2 className="text-2xl">
             <span className="font-bold bg-amber-500 text-gray-900 py-1 px-2 rounded-lg mr-1">
@@ -16,9 +17,8 @@ export default function Header() {
             <span className="text-xl hidden sm:inline">Clone</span>
           </h2>
         </Link>
+        <DarkModeSwitch />
       </div>
-
-      <div className=""></div>
       <div className="flex">
         <MenuItem title="Home" url="/" icon={<AiFillHome />} />
         <MenuItem title="About" url="/about" icon={<BsFillInfoCircleFill />} />
